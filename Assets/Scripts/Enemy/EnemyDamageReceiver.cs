99b134b4c371a9a0249b516ac6ac1e7bdfd0c00b
MCMrARM <mcmrarm@gmail.com>
2022-01-06 14:30:45 +0100

    Enemy: track damage history

--- Assets/Scripts/Enemy/EnemyDamageReceiver.cs
+++ Assets/Scripts/Enemy/EnemyDamageReceiver.cs
@@ -8,10 +8,15 @@ namespace Mahou
     {
         public StatManager statManager;
 
+        public delegate void DamageReceivedAction(DamageType type, float dmg, float finalDmg, StatManager attacker);
+        public DamageReceivedAction DamageReceived;
+
         public override void OnReceiveDamage(DamageType type, float dmg, StatManager attacker)
         {
             var finalDmg = statManager.CalculateDamage(type, dmg, attacker);
             statManager.hp = Mathf.Max(statManager.hp - finalDmg, 0);
+            if (DamageReceived != null)
+                DamageReceived(type, dmg, finalDmg, attacker);
         }
     }
 
--- Assets/Scripts/EnemyAI/AITargetSelector.cs
+++ Assets/Scripts/EnemyAI/AITargetSelector.cs
@@ -1,22 +1,109 @@
-﻿using UnityEngine;
+﻿using Mahou.Combat;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace Mahou.EnemyAI
 {
-    [RequireComponent(typeof(AIFoeLocator))]
+    [RequireComponent(typeof(AIFoeLocator), typeof(EnemyDamageReceiver))]
     class AITargetSelector : MonoBehaviour
     {
 
-        // TODO: Damage based target selection
-
         private AIFoeLocator _foeLocator;
+        private EnemyDamageReceiver _damageReceiver;
+
+        public int damageFrameCount = 5;
+        public float damageFrameTime = 3f;
+
+        private Dictionary<StatManager, float>[] _damageFrames;
+        private Dictionary<StatManager, float> _damageSum = new Dictionary<StatManager, float>();
+        private int _currentDamageFrame = -1;
+        private float _currentDamageFrameEnd = 0;
+
+        private StatManager _topAttacker;
 
         private void Start()
         {
             _foeLocator = GetComponent<AIFoeLocator>();
+            _damageFrames = new Dictionary<StatManager, float>[damageFrameCount];
+            _currentDamageFrameEnd = Time.time;
+        }
+
+        private void OnEnable()
+        {
+            _damageReceiver = GetComponent<EnemyDamageReceiver>();
+            _damageReceiver.DamageReceived += OnDamageReceived;
+        }
+
+        private void OnDisable()
+        {
+            _damageReceiver.DamageReceived -= OnDamageReceived;
+        }
+
+        private void AdvanceDamageFrame()
+        {
+            _currentDamageFrame = (_currentDamageFrame + 1) % damageFrameCount;
+            if (_damageFrames[_currentDamageFrame] != null)
+            {
+                foreach (var e in _damageFrames[_currentDamageFrame])
+                {
+                    _damageSum[e.Key] -= e.Value;
+                    if (_damageSum[e.Key] == 0)
+                        _damageSum.Remove(e.Key);
+                }
+                _damageFrames[_currentDamageFrame].Clear();
+            }
+            _topAttacker = null;
+        }
+
+        private void AdvanceDamageFrames()
+        {
+            if (Time.time >= _currentDamageFrameEnd)
+            {
+                int advanceCount = (int)((Time.time - _currentDamageFrameEnd) / damageFrameTime);
+                advanceCount = Math.Min(advanceCount, damageFrameCount);
+                for (; advanceCount > 0; --advanceCount)
+                    AdvanceDamageFrame();
+                _currentDamageFrameEnd += advanceCount * damageFrameTime;
+            }
+        }
+
+        private void OnDamageReceived(DamageType type, float dmg, float finalDmg, StatManager attacker)
+        {
+            AdvanceDamageFrames();
+            if (_damageFrames[_currentDamageFrame] == null)
+                _damageFrames[_currentDamageFrame] = new Dictionary<StatManager, float>();
+            float prevTotalDmg = 0f;
+            _damageFrames[_currentDamageFrame].TryGetValue(attacker, out prevTotalDmg);
+            _damageFrames[_currentDamageFrame][attacker] = prevTotalDmg + finalDmg;
+            _damageSum.TryGetValue(attacker, out prevTotalDmg);
+            _damageSum[attacker] = prevTotalDmg + finalDmg;
+            if (_topAttacker != null && _topAttacker != attacker && _damageSum[attacker]  > _damageSum[_topAttacker])
+                _topAttacker = null;
+        }
+
+        private void UpdateTopAttacker()
+        {
+            AdvanceDamageFrames();
+            if (_topAttacker == null && _damageSum.Count > 0)
+            {
+                float maxVal = -1f;
+                foreach (var e in _damageSum)
+                {
+                    if (e.Value > maxVal)
+                    {
+                        _topAttacker = e.Key;
+                        maxVal = e.Value;
+                    }
+                }
+            }
         }
 
         public GameObject GetTarget()
         {
+            UpdateTopAttacker();
+            if (_topAttacker != null)
+                return _topAttacker.gameObject;
             return _foeLocator.GetClosestFoe();
         }
 
