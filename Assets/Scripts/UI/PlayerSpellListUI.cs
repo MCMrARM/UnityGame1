36a4165ca8f6ac6a288236935402e05006c3a403
MCMrARM <mcmrarm@gmail.com>
2022-01-13 23:50:46 +0100

    Highlight selected spell

--- Assets/Scripts/UI/PlayerSpellListUI.cs
+++ Assets/Scripts/UI/PlayerSpellListUI.cs
@@ -6,36 +6,50 @@ namespace Mahou.UI
     public class PlayerSpellListUI : MonoBehaviour
     {
         public PlayerSpellListController spellListController;
-        private GameObject[] _uiSpellInfoObjects;
+        private SpellInfoUI[] _uiSpellInfo;
         public GameObject spellInfoPrefab;
         public float itemSpacing;
+        private int _uiActiveSpellIndex = -1;
 
         void Start()
         {
             RecreateElements();
         }
 
+        private void Update()
+        {
+            var index = spellListController.activeSpellIndex;
+            if (_uiActiveSpellIndex != -1)
+            {
+                _uiSpellInfo[_uiActiveSpellIndex].SetSelected(false);
+            }
+            _uiSpellInfo[index].SetSelected(true);
+            _uiActiveSpellIndex = index;
+        }
+
         private void RecreateElements()
         {
-            if (_uiSpellInfoObjects != null)
+            if (_uiSpellInfo != null)
             {
-                foreach (var obj in _uiSpellInfoObjects)
+                foreach (var obj in _uiSpellInfo)
                     Destroy(obj);
             }
 
             var caster = spellListController.GetComponent<SpellCaster>();
             var itemCount = spellListController.spellList.Length;
             var itemHeight = spellInfoPrefab.GetComponent<RectTransform>().sizeDelta.y;
-            _uiSpellInfoObjects = new GameObject[itemCount];
+            GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, (itemHeight + itemSpacing) * itemCount - itemSpacing);
+
+            _uiSpellInfo = new SpellInfoUI[itemCount];
             for (var i = 0; i < itemCount; i++)
             {
                 var spellInfo = Instantiate(spellInfoPrefab, transform);
                 spellInfo.transform.localPosition += new Vector3(0, -(itemHeight + itemSpacing) * i);
                 var uiComponent = spellInfo.GetComponent<SpellInfoUI>();
+                uiComponent.SavePositionForIndent();
                 uiComponent.SetSpell(caster, spellListController.spellList[i]);
-                _uiSpellInfoObjects[i] = spellInfo;
+                _uiSpellInfo[i] = uiComponent;
             }
-            GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, (itemHeight + itemSpacing) * itemCount - itemSpacing);
         }
     }
 
--- Assets/Scripts/UI/SpellInfoUI.cs
+++ Assets/Scripts/UI/SpellInfoUI.cs
@@ -10,6 +10,14 @@ namespace Mahou.UI
     {
         public TMPro.TextMeshProUGUI nameLabel;
         public RawImage iconImage;
+        public GameObject activeArrowImage;
+        private Vector3 _basePos;
+        public float activeIndent;
+
+        public void SavePositionForIndent()
+        {
+            _basePos = transform.localPosition;
+        }
 
         public void SetSpell(SpellCaster caster, SpellConfig spell)
         {
@@ -19,6 +27,16 @@ namespace Mahou.UI
 
             nameLabel.text = spell.uiName;
             iconImage.texture = spell.uiIcon;
+            activeArrowImage.SetActive(false);
+        }
+        
+        public void SetSelected(bool selected)
+        {
+            var newPos = _basePos;
+            if (selected)
+                newPos.x -= activeIndent;
+            transform.localPosition = newPos;
+            activeArrowImage.SetActive(selected);
         }
     }
 }
