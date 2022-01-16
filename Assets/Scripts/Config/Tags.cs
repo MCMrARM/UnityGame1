0c4787b17014b244102b7392ce73a9217805925a
MCMrARM <mcmrarm@gmail.com>
2022-01-16 16:25:05 +0100

    Do not cancel out projectiles

--- Assets/Scripts/Config/Tags.cs
+++ Assets/Scripts/Config/Tags.cs
@@ -3,10 +3,11 @@
     struct Tags
     {
         public const string PLAYER = "Player";
+        public const string PROJECTILE = "Projectile";
 
         public static bool IsValidTarget(string tag)
         {
-            return true;
+            return tag != PROJECTILE;
         }
     }
 }
